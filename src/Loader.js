9279ba57cee5ee28b929f8e564dd32aa944c09ae
Nicholas Alan Brown <nicholasalanbrown@gmail.com>
2019-02-22 22:23:39 -0500

    Add Loader

---
+++ src/Loader.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Sentry } from 'react-activity'
+import 'react-activity/dist/react-activity.css'
+import theme from './theme'
+
+export default class Loader extends React.Component {
+  render () {
+    const { color } = this.props
+    return (
+      <Sentry color={color} size={35}
+        speed={0.7} />
+    )
+  }
+}
+
+Loader.displayName = 'Loader'
+
+Loader.defaultProps = {
+  color: theme.colors.primary
+}
+
+Loader.propTypes = {
+  color: PropTypes.string
+}
