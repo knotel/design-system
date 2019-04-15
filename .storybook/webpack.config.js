f4baec3ec6e34243b57a18740a75d36b0a82215c
Justin Michaliga <justin.michaliga@gmail.com>
2019-04-15 12:17:30 -0400

    feat: dot storybook

---
+++ .storybook/webpack.config.js
@@ -0,0 +1,20 @@
+// const path = require('path')
+
+// module.exports = {
+//   resolve: {
+//     alias: {
+//       'styled-components': path.join(
+//         __dirname,
+//         '../node_modules/styled-components'
+//       ),
+//       'styled-system': path.join(__dirname, '../node_modules/styled-system'),
+//       'prop-types': path.join(__dirname, '../node_modules/prop-types'),
+//       '@babel/runtime': path.join(__dirname, '../node_modules/@babel/runtime'),
+//       'pcln-design-system': path.join(__dirname, '../packages/core/src/index'),
+//       'pcln-icons': path.join(__dirname, '../packages/icons')
+//     }
+//   }
+// }
+
+module.exports = async ({ config }) =>
+  console.dir(config.plugins, { depth: null }) || config
