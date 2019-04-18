1e714a508607a5c0ffeb1383840dc1f6f00df5f6
Justin Michaliga <justin.michaliga@gmail.com>
2019-04-18 14:19:41 -0400

    responsive text on responsive example

--- packages/core/storybook/ImageHover.js
+++ packages/core/storybook/ImageHover.js
@@ -18,8 +18,8 @@ storiesOf('ImageHover', module)
         image="https://www.priceline.com/home/public/assets/images/photos/photo-aruba.jpg"
       >
         <Box p={4}>
-          <Text fontSize={6} bold textAlign="center" color="white">
-            What's up doc?
+          <Text fontSize={4} bold textAlign="center" color="white">
+            Zoom. Zoom.
           </Text>
         </Box>
       </ImageHover>
@@ -32,14 +32,18 @@ storiesOf('ImageHover', module)
       image="https://www.priceline.com/home/public/assets/images/photos/photo-aruba.jpg"
     >
       <Box p={4}>
-        <Text fontSize={6} bold textAlign="center" color="white">
-          What's up doc?
+        <Text fontSize={4} bold textAlign="center" color="white">
+          Zoom. Zoom.
         </Text>
       </Box>
     </ImageHover>
   ))
   .add('Responsive width, with Box', () => (
-    <Box width={1 / 2}>
-      <ImageHover image="https://www.priceline.com/home/public/assets/images/photos/photo-aruba.jpg" />
+    <Box width={[1 / 2, 1 / 3, 1 / 4]}>
+      <ImageHover image="https://www.priceline.com/home/public/assets/images/photos/photo-aruba.jpg">
+        <Text fontSize={[3, 4, 5]} bold textAlign="center" color="white">
+          Zoom. Zoom.
+        </Text>
+      </ImageHover>
     </Box>
   ))
