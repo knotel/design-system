328b9ded763ddd92c275347fc646d92ec7cecb63
Justin Michaliga <justin.michaliga@gmail.com>
2019-04-22 12:59:37 -0400

    feat: more complex storybook

--- packages/dropdown/storybook/Dropdown.js
+++ packages/dropdown/storybook/Dropdown.js
@@ -1,9 +1,17 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
 import Dropdown from '../src/index'
+import Slider from '../src/../../slider/src'
 
-storiesOf('Dropdown', module).add('Basic', () => (
-  <Dropdown title="Anything">
-    <p>Hi</p>
-  </Dropdown>
-))
+storiesOf('Dropdown', module)
+  .add('Basic', () => (
+    <Dropdown title="Hello">
+      <p>Hello</p>
+    </Dropdown>
+  ))
+  .add('Complex', () => (
+    <Dropdown title="What is your budget?">
+      <Slider value={[8, 70]} color="green" mb={2} />
+      <Slider value={[16, 64]} color="red" mb={2} />
+    </Dropdown>
+  ))
