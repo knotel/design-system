b0b079559bc1921e881efa9580555075666c9965
Justin Michaliga <justin.michaliga@gmail.com>
2019-05-04 15:06:20 -0400

    feat: fix tabs, remove datepicker

--- packages/tabs/src/index.js
+++ packages/tabs/src/index.js
@@ -1,226 +1,66 @@
-import React, { Component } from 'react'
-import Swipeable from 'react-tabs'
-import { Box, Flex, Relative } from 'pcln-design-system'
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Flex, Text } from 'pcln-design-system'
 import styled from 'styled-components'
 
-class Carousel extends Component {
+const TabTitle = styled(Text)`
+  border-bottom: ${props =>
+    props.isActive ? `4px solid ${props.activeColor}` : 'none'};
+  cursor: pointer;
+`
+
+class Tabs extends React.Component {
   constructor(props) {
     super(props)
 
     this.state = {
-      items: this.props.children,
-      internalCurrentItemIndex: this.props.currentItemIndex || 0,
-      currentItemTranslate: 0,
-      isSwiping: false
+      activeTab: 0
     }
-
-    this.handleSwipingLeft = this.handleSwipingLeft.bind(this)
-    this.handleSwipingRight = this.handleSwipingRight.bind(this)
-    this.handleSwipedLeft = this.handleSwipedLeft.bind(this)
-    this.handleSwipedRight = this.handleSwipedRight.bind(this)
-    this.updateSelectedItem = this.updateSelectedItem.bind(this)
-    this.mapItems = this.mapItems.bind(this)
-    this.mapDots = this.mapDots.bind(this)
-  }
-
-  handleSwipingLeft(e, absX) {
-    const swipedPercentage = (absX / window.innerWidth) * 100
-    const swipeLeft = swipedPercentage <= 100 ? swipedPercentage : 100
-    this.setState(previousState => ({
-      currentItemTranslate: 0 - swipeLeft,
-      isSwiping: true
-    }))
-  }
-
-  handleSwipingRight(e, absX) {
-    const swipedPercentage = (absX / window.innerWidth) * 100
-    const swipeRight = swipedPercentage <= 100 ? swipedPercentage : 100
-    this.setState(previousState => ({
-      currentItemTranslate: 0 + swipeRight,
-      isSwiping: true
-    }))
-  }
-
-  handleSwipedLeft(e, absX) {
-    const index =
-      this.props.currentItemIndex || this.state.internalCurrentItemIndex
-    const nextItemIndex = (index + 1) % this.state.items.length
-
-    this.setState({
-      internalCurrentItemIndex: nextItemIndex,
-      currentItemTranslate: 0,
-      isSwiping: false
-    })
-  }
-
-  handleSwipedRight(e, absX) {
-    const index =
-      this.props.currentItemIndex || this.state.internalCurrentItemIndex
-    const previousItemIndex =
-      (index + this.state.items.length - 1) % this.state.items.length
-
-    this.setState({
-      internalCurrentItemIndex: previousItemIndex,
-      currentItemTranslate: 0,
-      isSwiping: false
-    })
-  }
-
-  updateSelectedItem(selectedItemIndex) {
-    this.setState({
-      internalCurrentItemIndex: selectedItemIndex
-    })
-  }
-
-  mapItems() {
-    const { items } = this.state
-
-    const currentItemIndex =
-      this.props.currentItemIndex || this.state.internalCurrentItemIndex
-
-    const itemsToShow = []
-    items.forEach((item, index) => {
-      if (index <= currentItemIndex + 2) {
-        itemsToShow.push(
-          <span key={`item-${index}`}>
-            <PositionItem>{item}</PositionItem>
-          </span>
-        )
-      }
-    })
-
-    return itemsToShow
-  }
-
-  mapDots() {
-    const currentItemIndex =
-      this.props.currentItemIndex || this.state.internalCurrentItemIndex
-
-    return this.state.items.map((item, mapIndex) => (
-      <Dot
-        key={mapIndex}
-        mx={2}
-        mt={2}
-        bg={mapIndex === currentItemIndex ? 'blue' : 'borderGray'}
-        onClick={this.updateSelectedItem.bind(this, mapIndex)}
-      />
-    ))
-  }
-
-  componentWillReceiveProps(nextProps) {
-    this.setState({
-      items: nextProps.children
-    })
   }
 
   render() {
-    const { currentItemTranslate, isSwiping } = this.state
-
-    const { shouldShowArrows } = this.props
-
-    const currentItemIndex =
-      this.props.currentItemIndex || this.state.internalCurrentItemIndex
+    const { activeColor, defaultColor, tabs } = this.props
+    const children = React.Children.toArray(this.props.children)
 
     return (
-      <Box {...this.props}>
-        <OverflowContainer w={1} mb={3}>
-          {shouldShowArrows && (
-            <div>
-              <PositionedArrowLeftController
-                handleClick={this.handleSwipedRight}
-                iconDirection="chevronLeft"
-              />
-              <PositionedArrowRightController
-                handleClick={this.handleSwipedLeft}
-                iconDirection="chevronRight"
-              />
-            </div>
-          )}
-          <Swipeable
-            onSwipingLeft={this.handleSwipingLeft}
-            onSwipingRight={this.handleSwipingRight}
-            onSwipedLeft={this.handleSwipedLeft}
-            onSwipedRight={this.handleSwipedRight}
-            trackMouse
-          >
-            <SwiperWrap
-              translateX={-92 * currentItemIndex + currentItemTranslate + 4}
-              isSwiping={isSwiping}
+      <React.Fragment>
+        <Flex bg="lightGray" px={1} pt={2}>
+          {tabs.map((tab, i) => (
+            <TabTitle
+              color={this.state.activeTab === i ? activeColor : defaultColor}
+              key={i}
+              mx={3}
+              py={2}
+              onClick={() => this.setState({ activeTab: i })}
+              isActive={this.state.activeTab === i}
+              {...this.props}
             >
-              {this.mapItems()}
-            </SwiperWrap>
-          </Swipeable>
-        </OverflowContainer>
-        <Flex wrap justify="center">
-          {this.mapDots()}
+              {tab.title}
+            </TabTitle>
+          ))}
         </Flex>
-      </Box>
+        <Flex bg="white" py={3} px={3}>
+          {children[this.state.activeTab]}
+        </Flex>
+      </React.Fragment>
     )
   }
 }
 
-const OverflowContainer = styled(Relative)`
-  overflow: hidden;
-`
-
-const Dot = styled(Box)`
-  height: 12px;
-  width: 12px;
-  border-radius: 50%;
-  cursor: pointer;
-`
-
-Dot.displayName = 'Dot'
-
-const PositionedArrowLeftController = styled(ArrowController)`
-  position: absolute;
-  top: 38%;
-  z-index: 1;
-`
-
-const PositionedArrowRightController = styled(PositionedArrowLeftController)`
-  right: 0;
-`
-
-const SwiperWrap = styled.div`
-  transition-property: all;
-  transition-duration: ${props => (props.isSwiping ? '0s' : '0.25s')};
-  transition-timing-function: ease-in-out;
-  transition-delay: initial;
-  left: 0;
-  right: 0;
-  top: 0;
-  bottom: 0;
-  padding: 0;
-  white-space: nowrap;
-  transform: ${props => `translate(${props.translateX}%, 0)`};
-`
-
-const PositionItem = styled(Box)`
-  display: inline-flex;
-  width: 92%;
-  object-fit: contain;
-
-  transition: opacity 0.3s ease 0s;
-  user-select: none;
-  * {
-    user-drag: none;
-  }
-
-  > * {
-    width: 100%;
-    white-space: normal;
-  }
-`
-
-// Carousel.propTypes = {
-//   children: PropTypes.arrayOf(PropTypes.node).isRequired,
-//   currentItemIndex: PropTypes.number,
-//   shouldShowArrows: PropTypes.bool
-// }
-
-Carousel.defaultProps = {
+Tabs.defaultProps = {
+  tabs: [],
   children: []
 }
 
-export default Carousel
+Tabs.propTypes = {
+  activeColor: PropTypes.string,
+  defaultColor: PropTypes.string,
+  tabs: PropTypes.arrayOf(
+    PropTypes.shape({
+      title: PropTypes.string.isRequired
+    })
+  ).isRequired,
+  children: PropTypes.arrayOf(PropTypes.node).isRequired
+}
+
+export default Tabs
--- packages/tabs/storybook/Tabs.js
+++ packages/tabs/storybook/Tabs.js
@@ -1,27 +1,111 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
+import { Box, Flex, Heading } from 'pcln-design-system'
 import Tabs from '../src'
-import { Image } from 'pcln-design-system'
 
-const images = [
-  'https://via.placeholder.com/150/abcdef/111111?text=1',
-  'https://via.placeholder.com/150/abcdef/111111?text=2',
-  'https://via.placeholder.com/150/abcdef/111111?text=3'
+const props = {
+  activeColor: 'darkBlue',
+  defaultColor: 'blue',
+  tabs: [
+    {
+      title: 'Tab 1'
+    },
+    {
+      title: 'Tab 2'
+    },
+    {
+      title: 'Tab 3'
+    }
+  ]
+}
+
+const childElements = [
+  <Box>Child 1</Box>,
+  <Box>Child 2</Box>,
+  <Box>Child 3</Box>
 ]
 
-const items = images.map((image, index) => <Image key={index} src={image} />)
+const mappedProps = [
+  {
+    title: 'Mapped content 1',
+    body: 'Body Text 1'
+  },
+  {
+    title: 'Mapped content 2',
+    body: 'Body Text 2'
+  },
+  {
+    title: 'Mapped content 3',
+    body: 'Body Text 3'
+  }
+]
 
-const story = storiesOf('Tabs', module).add('Simple', () => {
-  return (
-    <Tabs defaultIndex={1} onSelect={index => console.log(index)}>
-      <TabList>
-        <Tab>Title 1</Tab>
-        <Tab>Title 2</Tab>
-      </TabList>
-      <TabPanel />
-      <TabPanel />
-    </Tabs>
-  )
-})
+const ExampleTab = props => (
+  <Flex flexDirection="column">
+    <Heading>{props.title}</Heading>
+    <Box>{props.body}</Box>
+  </Flex>
+)
 
-export default story
+const mockHotelProps = {
+  activeColor: 'darkBlue',
+  defaultColor: 'blue',
+  tabs: [
+    {
+      title: 'Tab 1',
+      content: {
+        confirmationNumber: '123-TEST'
+      }
+    },
+    {
+      title: 'Tab 2',
+      content: {
+        hotelName: 'Venetian Las Vegas'
+      }
+    },
+    {
+      title: 'Tab 3',
+      content: {
+        importantInfo: 'Free wifi'
+      }
+    }
+  ]
+}
+
+const ManageTab = props => <Box>{props.confirmationNumber}</Box>
+
+const RoomTab = props => <Box>{props.hotelName}</Box>
+
+const ImportantInfoTab = props => <Box>{props.importantInfo}</Box>
+
+storiesOf('Tabs', module)
+  .add('with hardcoded elements', () => {
+    return (
+      <Tabs {...props}>
+        <Box>Test 1</Box>
+        <Box>Test 2</Box>
+        <Box>Test 3</Box>
+      </Tabs>
+    )
+  })
+  .add('with mapped elements', () => {
+    return <Tabs {...props}>{childElements.map(child => child)}</Tabs>
+  })
+  .add('with mapped props', () => {
+    return (
+      <Tabs {...props}>
+        {mappedProps.map((content, i) => (
+          <ExampleTab key={i} {...content} />
+        ))}
+      </Tabs>
+    )
+  })
+  .add('with composite components consuming props', () => {
+    return (
+      <Tabs {...mockHotelProps}>
+        <ManageTab {...mockHotelProps.tabs[0].content} />
+        <RoomTab {...mockHotelProps.tabs[1].content} />
+        <ImportantInfoTab {...mockHotelProps.tabs[2].content} />
+      </Tabs>
+    )
+  })
